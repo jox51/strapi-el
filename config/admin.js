1cdaccf2fb86a6177939624bf4be52dcbdd2b04e
Orlando L Dominici <orlandoldominici@Orlandos-Air.home>
2023-02-18 10:43:39 -0800

    update admin file

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("5D4MfdmVi8tp2IYON2CLXA"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("F9mEalY5nEQfMXILVRXfBA"),
   },
 });
